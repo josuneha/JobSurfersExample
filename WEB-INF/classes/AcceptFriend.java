e907754c63a88eee6b947363bd3f74d0cd3827b3
jhernantes <jhernantes@gmail.com>
2017-03-21 15:35:25 +0100

    new sentence has added to see how to commit
    example of a commit 2

--- WEB-INF/classes/AcceptFriend.java
+++ WEB-INF/classes/AcceptFriend.java
@@ -22,12 +22,16 @@ public class AcceptFriend extends HttpServlet {
         HttpSession session = req.getSession(false);
         String userReceives = (String)session.getAttribute("user");
         String userSends = req.getParameter("sender");
+		
 
         String sql = "INSERT INTO Friends (Friend1, Friend2) VALUES (";
         sql +=  "'" + userSends + "'";
         sql +=  ", '" + userReceives + "')"; 
         System.out.println("Insert sql: " + sql);
         
+		System.out.println("Example of commit: ");
+        
+		
         try {
             Statement statementInsert=connection.createStatement();
             int num = statementInsert.executeUpdate(sql);
